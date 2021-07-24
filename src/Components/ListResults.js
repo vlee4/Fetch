9e4154977d91c8f94a93343af226e6910297d424
vlee4 <vicglee1234007@gmail>
2021-07-24 17:47:16 -0500

    Create files for expected components (#1)

---
+++ src/Components/ListResults.js
@@ -0,0 +1,11 @@
+//ListResults Component: will display list of dog breeds (all or searched)
+
+import React from 'react';
+
+const ListResults = () => {
+    return (
+        <div>This will list results</div>
+    )
+}
+
+export default ListResults;
