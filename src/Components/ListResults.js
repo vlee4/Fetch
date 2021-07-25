56ee31bc4319c4c94fd5026ce5a07e4de17d7a39
vlee4 <vicglee1234007@gmail>
2021-07-24 20:28:40 -0500

    Map out dog breeds provided by GET req (#4)

--- src/Components/ListResults.js
+++ src/Components/ListResults.js
@@ -1,10 +1,31 @@
 //ListResults Component: will display list of dog breeds (all or searched)
 
-import React from "react";
+import React,{Fragment} from "react";
 
-const ListResults = () => {
+const ListResults = ({breeds}) => {
+    console.log("breeds", breeds)
     return (
-        <div>This will list results</div>
+        <div>This will list results
+            <div>{Object.entries(breeds).map(([breed,subBreed], idx)=>{
+                if(subBreed){
+                    return(
+                        <Fragment key={`dog_breed_${idx}`}>
+                        <ul>Breed: {breed}</ul>
+                        <>{subBreed.map(sub=>{
+                            return(
+                                <li>Sub-breed: {sub}</li>
+                            )
+                        })}</>
+                    </Fragment>
+                    )
+                }
+                else{
+                    return (
+                        <ul key={`dog_breed_${idx}`}>Breed: {breed}</ul>
+                    )
+                }
+            })}</div>
+        </div>
     )
 }
 
