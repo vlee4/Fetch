a80a5fc72ff64cb2e9b2d43e06e9efe843eae6d0
vlee4 <vicglee1234007@gmail>
2021-07-24 23:23:09 -0500

    Fetch breed-specific vs sub-breed-specific dog pictures via different endpoints calls & map them out once stored in state (#6)

--- src/Components/SpecificBreed.js
+++ src/Components/SpecificBreed.js
@@ -1,10 +1,41 @@
 //SpecificBreed Component: Will display 4 random dog pictures of a specific breed + 'Back' button
 
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import axios from 'axios';
 
 const SpecificBreed = () => {
+    const params = useParams();
+    const [pictures, setPictures] = useState([]);
+
+    useEffect(()=>{
+        const fetchPictures = async() =>{
+            try{
+                //If the the user is viewing a sub-breed, the component will make a GET request for 4 random pictures of the sub-breed
+                if(params.subBreed){
+                    const {data} = await axios.get(`https://dog.ceo/api/breed/${params.breed}/${params.subBreed}/images/random/4`);
+                    setPictures(data.message)
+                   
+                }else{ //If the user is viewing a general breed, the component will GET 4 random pictuers of the general breed
+                    const {data} = await axios.get(`https://dog.ceo/api/breed/${params.breed}/images/random/4`);
+                    setPictures(data.message)
+                }
+            }catch(err){
+                console.log(err)
+            }
+        }
+        fetchPictures()
+    },[pictures, params])
+  
     return (
-        <div>This will display a specific breed</div>
+        <div>
+            <h2>{params.breed} {params.subBreed?params.subBreed:""}</h2>
+            {pictures?.map((picURL, idx)=>{
+                return(
+                    <img key={`dog_${idx}`} src={picURL}></img>
+                )
+            })}
+        </div>
     )
 }
 
