4a8e251a48afc257ed9f64a4ae756ed3113d11ff
vlee4 <vicglee1234007@gmail>
2021-07-24 19:12:05 -0500

    Use axios to make GET request for all dog breeds (#3)

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,27 @@
 //Homepage Component
 
-import React from 'react';
+import React, {useEffect} from "react";
+import ListResults from "./ListResults"
+import axios from 'axios';
 
 const Home = () => {
+
+useEffect(()=>{
+    const fetchDogs = async() =>{
+        try{
+            const {data} = await axios.get("https://dog.ceo/api/breeds/list/all")  
+            console.log('dog breeds:', data.message)
+        }catch(err){
+            console.log(err)
+        }
+    }
+    fetchDogs()
+},[])
+
     return (
-        <div>This is the Homepage</div>
+        <div>This is the Homepage
+            <ListResults/>
+        </div>
     )
 }
 
