ed30cd4bf9662aee58d3026b66ede445b384ecf3
vlee4 <vicglee1234007@gmail>
2021-07-24 20:38:54 -0500

    Link breed names to corresponding SpecificBreed page (#4)

--- src/Components/ListResults.js
+++ src/Components/ListResults.js
@@ -1,16 +1,18 @@
 //ListResults Component: will display list of dog breeds (all or searched)
 
 import React,{Fragment} from "react";
+import {Link} from "react-router-dom";
 
 const ListResults = ({breeds}) => {
     console.log("breeds", breeds)
     return (
         <div>This will list results
             <div>{Object.entries(breeds).map(([breed,subBreed], idx)=>{
-                if(subBreed){
+                //If the breed has a sub-breed array it will display as a list item
+                if(subBreed.length){
                     return(
                         <Fragment key={`dog_breed_${idx}`}>
-                        <ul>Breed: {breed}</ul>
+                        <Link to={`/${breed}`}><ul>Breed: {breed}</ul></Link>
                         <>{subBreed.map(sub=>{
                             return(
                                 <li>Sub-breed: {sub}</li>
@@ -21,7 +23,7 @@ const ListResults = ({breeds}) => {
                 }
                 else{
                     return (
-                        <ul key={`dog_breed_${idx}`}>Breed: {breed}</ul>
+                        <Link to={`/${breed}`}><ul key={`dog_breed_${idx}`}>Breed: {breed}</ul></Link>
                     )
                 }
             })}</div>
