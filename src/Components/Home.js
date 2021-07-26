4e59cc4eabf6376b5a668eea6affcc2d55034e5d
vlee4 <vicglee1234007@gmail>
2021-07-26 01:20:51 -0500

    Setup structure for filtering ListResults (#9)

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,16 @@
 //Homepage Component
-
 import React, {useEffect, useState} from "react";
-import ListResults from "./ListResults"
 import axios from "axios";
+
+//COMPONENTS
+import ListResults from "./ListResults"
 
 const Home = () => {
 const [breeds, setBreeds] = useState({})
+
+const handleFilter = () => {
+
+}
 
 useEffect(()=>{
     const fetchDogs = async() =>{
@@ -21,6 +26,10 @@ useEffect(()=>{
 
     return (
         <div>This is the Homepage
+            <form>
+               <label>Filter dog breeds:  <input type="search" aria-label="Filter dog breeds"></input></label>
+                <button type="button" className="btn btn-primary m-1">Search</button>
+            </form>
             <ListResults breeds={breeds}/>
         </div>
     )
