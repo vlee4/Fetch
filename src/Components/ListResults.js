5fe2e6a9ba6ae983f4d7d61ad75e35509ad6ba5f
vlee4 <vicglee1234007@gmail>
2021-07-26 19:01:48 -0500

    Add shadow to distinguish breed & sub-breeds (#7)

--- src/Components/ListResults.js
+++ src/Components/ListResults.js
@@ -8,13 +8,13 @@ const ListResults = ({breeds}) => {
     return (
         <div className="container">
             <div className="d-flex justify-content-center">
-                <ul className="list-results list-group d-sm-flex flex-column align-items-start d-md-grid px-md-5 py-md-3">{Object.entries(breeds).map(([breed,subBreed], idx)=>{
+                <ul className="list-results list-group d-sm-flex flex-column align-items-center d-md-grid px-md-5 py-md-3">{Object.entries(breeds).map(([breed,subBreed], idx)=>{
                     //If the breed has a sub-breed array it will display as a list item link
                     if(subBreed.length){
                         return(
-                            <span className="list-group-item" key={`dog_breed_${idx}`}>
+                            <span className="list-group-item breed-wrapper" key={`dog_breed_${idx}`}>
                             <li className="breed">Breed: <Link to={`/breed/${breed}`}>{breed}</Link></li>
-                            <ul>{subBreed.map((sub, idx)=>{
+                            <ul className="ps-0">{subBreed.map((sub, idx)=>{
                                 return(
                                     <li className="sub-breed" key={`dog_subBreed_${idx}`}>Sub-breed: <Link to={`/breed/${breed}/${sub}`}>{sub}</Link></li>
                                 )
