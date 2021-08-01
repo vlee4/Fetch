60e2e03c5ee6929606d3c0abe85454347de605fe
vlee4 <vicglee1234007@gmail>
2021-08-01 14:47:36 -0500

    Set up action to reducer pipeline accounting for components' state changes (#15)

--- src/Store/results.js
+++ src/Store/results.js
@@ -1,16 +1,56 @@
 //results state
+import axios from "axios";
 
 //ACTIONS
-const GET_RESULTS = "GET_RESULTS";
+const GET_ALL_RESULTS = "GET_ALL_RESULTS";
 const SET_FILTER = "SET_FILTER";
-const SET_SEARCHED = "SET_SEARCHED"
+const SET_SEARCHED = "SET_SEARCHED";
 
 //ACTION CREATORS
+const getAllResults = (results) => {
+    return{
+        type: GET_ALL_RESULTS,
+        results
+    }
+}
+
+const getFiltered = (filter, filterResults) => {
+    return {
+        type: GET_FILTERED,
+        filter,
+        filterResults
+    }
+}
+
+const setSearchState = (state) => {
+    return{
+        type: SET_SEARCHED,
+        state
+    }
+}
+
+//THUNK (CREATORS)
+export const fetchResults = () => {
+    return async dispatch => {
+        try{
+            const {data} = axios.get("https://dog.ceo/api/breeds/list/all")
+            dispatch(getResults(data.message))
+        }catch(err){
+            console.log(err)
+        }
+    }
+}
 
 //REDUCER
 
 const resultsReducer = (state={}, action) =>{
     switch(action.type){
+        case GET_RESULTS:
+            return {...state, results: action.results}
+        case GET_FILTERED:
+            return {...state, filter: action.filter, filteredResults: action.filterResults}
+        case SET_SEARCHED:
+            return {...state, searched: action.state}
         default: 
             return state;
     }
