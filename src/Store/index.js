bd76f1341b3dad32610bd7871c95e23656d8a010
vlee4 <vicglee1234007@gmail>
2021-08-01 14:17:06 -0500

    Apply thunk middleware (#15)

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,11 +1,17 @@
-//Redux Store
-
+//Redux Store: Sets up Redux to provide global state throughout the app
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import ThunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import results from "./results"
 
 const reducer = combineReducers({
     results
 })
 
-const store = createStore(reducer)
+const middleware = composeWithDevTools(
+    applyMiddleware(ThunkMiddleware)
+)
+
+const store = createStore(reducer, middleware)
 export default store;
