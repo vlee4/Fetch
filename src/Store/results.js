32f1801c5be4ed1c6aa94a0e247130526326e662
vlee4 <vicglee1234007@gmail>
2021-08-01 19:30:02 -0500

    Separate filter & filteredResults as separate concerns (#15)

--- src/Store/results.js
+++ src/Store/results.js
@@ -4,6 +4,7 @@ import axios from "axios";
 //ACTIONS
 const GET_ALL_BREEDS = "GET_ALL_BREEDS";
 const SET_FILTER = "SET_FILTER";
+const SET_FILTERED_RESULTS = "SET_FILTERED_RESULTS";
 const SET_SEARCHED = "SET_SEARCHED";
 
 //ACTION CREATORS
@@ -14,15 +15,20 @@ const getAllBreeds = (breeds) => {
     }
 }
 
-const getFiltered = (filter, filterResults) => {
+export const setFilter = (filter) => {
     return {
         type: SET_FILTER,
         filter,
-        filterResults
+    }
+}
+export const setFilteredResults = (filteredResults) => {
+    return {
+        type: SET_FILTERED_RESULTS,
+        filteredResults,
     }
 }
 
-const setSearchState = (state) => {
+export const setSearchState = (state) => {
     return{
         type: SET_SEARCHED,
         state
@@ -34,7 +40,6 @@ export const fetchBreeds = () => {
     return async dispatch => {
         try{
             const {data} = await axios.get("https://dog.ceo/api/breeds/list/all")
-            console.log('data', data.message)
             dispatch(getAllBreeds(data.message))
         }catch(err){
             console.log(err)
@@ -57,7 +62,9 @@ const resultsReducer = (state=initialState, action) =>{
         case GET_ALL_BREEDS:
             return {...state, breeds: action.breeds}
         case SET_FILTER:
-            return {...state, filter: action.filter, filteredResults: action.filterResults}
+            return {...state, filter: action.filter}
+        case SET_FILTERED_RESULTS:
+            return {...state, filteredResults: action.filteredResults}
         case SET_SEARCHED:
             return {...state, searched: action.state}
         default: 
